54e8aae0b1239c601b83fe75a9071342172fccd9
kethavath aadarsh <95641315+letsCompete@users.noreply.github.com>
2023-03-13 04:45:08 +0530

    okaty

--- BMI.js
+++ BMI.js
@@ -1,10 +1,9 @@
 function calculateBMI() {
   var age = parseInt(document.getElementById("age").value);
   var weight = parseFloat(document.getElementById("weight").value);
-
+  var heightInMeters = parseInt(document.getElementById("Height").value);
   // Calculate BMI
-  var heightInMeters = 1.7; // assuming a height of 1.7 meters
-  var bmi = weight / (heightInMeters * heightInMeters);
+    var bmi = weight / (heightInMeters * heightInMeters);
 
   // Display result
   alert("Your BMI is " + bmi.toFixed(2));
