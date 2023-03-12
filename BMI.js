98bf5fdbce95f5b0e9ad20811bc63cabf5d5d83a
kethavath aadarsh <95641315+letsCompete@users.noreply.github.com>
2023-03-13 05:00:15 +0530

    motivation

--- BMI.js
+++ BMI.js
@@ -1,10 +1,29 @@
 function calculateBMI() {
   var age = parseInt(document.getElementById("age").value);
   var weight = parseFloat(document.getElementById("weight").value);
-  var heightInMeters = parseInt(document.getElementById("Height").value);
+  var heightInMeters = parseFloat(document.getElementById("height").value);
+
   // Calculate BMI
-    var bmi = weight / (heightInMeters * heightInMeters);
+  var bmi = weight / (heightInMeters * heightInMeters);
 
   // Display result
-  alert("Your BMI is " + bmi.toFixed(2));
+  var message;
+  if (bmi >= 30 && bmi < 31) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Eat once a day and walk, drink plenty of water.";
+  } else if (bmi >= 29 && bmi < 30) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Eat once a day, walk, do crunches, drink plenty of water.";
+  } else if (bmi >= 28 && bmi < 29) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Eat twice a day, walk, run, do crunches, drink plenty of water.";
+  } else if (bmi >= 27 && bmi < 28) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Eat twice a day, walk, run, do crunches and planks, drink plenty of water, get enough sleep.";
+  } else if (bmi >= 26 && bmi < 27) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Eat twice a day, have lunch, run, walk, do crunches and planks, drink plenty of water, get enough sleep.";
+  } else if (bmi >= 25 && bmi < 26) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Add extra meditation to your routine.";
+  } else if (bmi >= 24 && bmi < 25) {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Add extra eggs to your diet.";
+  } else {
+    message = "Your BMI is " + bmi.toFixed(2) + ". Hurray, it's party time!";
+  }
+  alert(message);
 }
