c518c5ce73a85b00a73e802823baa6fdb33fd78d
kethavath aadarsh <95641315+letsCompete@users.noreply.github.com>
2023-03-13 05:54:02 +0530

    my Ai model

--- BMI.js
+++ BMI.js
@@ -7,9 +7,29 @@ function calculateBMI() {
   var bmi = weight / (heightInMeters * heightInMeters);
 
   // Display result in modal dialog box
-  var resultText = "Your BMI is " + bmi.toFixed(2);
+  var resultText;
+  
+  if (bmi >= 31 && bmi < 32) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Eat once a day and walk, drink plenty of water.";
+   } else if (bmi >= 30 && bmi < 31) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Eat once a day,walk, drink plenty of water.";  
+  } else if (bmi >= 29 && bmi < 30) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Eat once a day, walk, do crunches, drink plenty of water.";
+  } else if (bmi >= 28 && bmi < 29) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Eat twice a day, walk, run, do crunches, drink plenty of water.";
+  } else if (bmi >= 27 && bmi < 28) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Eat twice a day, walk, run, do crunches and planks, drink plenty of water, get enough sleep.";
+  } else if (bmi >= 26 && bmi < 27) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Eat twice a day, have lunch, run, walk, do crunches and planks, drink plenty of water, get enough sleep.";
+  } else if (bmi >= 25 && bmi < 26) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Add extra meditation to your routine.";
+  } else if (bmi >= 24 && bmi < 25) {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Add extra eggs to your diet.";
+  } else {
+    resultText = "Your BMI is " + bmi.toFixed(2) + ". Hurray, it's party time!";
+  }
+
   var modalContent = '<div class="modal-dialog modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-body"><h1>' + resultText + '</h1></div></div></div>';
   var modal = $('<div class="modal" tabindex="-1" role="dialog">' + modalContent + '</div>');
   modal.modal();
 }
-
